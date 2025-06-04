11a74da43273a17d4406e04ba20c77c704452171
Navyasri <pellakuru_navyasri@epam.com>
2025-06-04 18:39:03 +0530

    changed output file

--- task08/modules/redis/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "redis_hostname" {
-  value = azurerm_redis_cache.redis.hostname
-}
-
-output "redis_access_key" {
-  value     = azurerm_redis_cache.redis.primary_access_key
-  sensitive = true
-}
